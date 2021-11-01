1b280eeb3ed9608dfd4d805447c94d2275dab57f
Cesar Rodriguez <kesarr@gmail.com>
2021-11-01 20:00:43 +0100

    🚀

--- src/utils/metamaskMobileDetector.js
+++ src/utils/metamaskMobileDetector.js
@@ -8,7 +8,8 @@ function isMetamaskMobileAndIframe() {
         (userAgent ===
           "Mozilla/5.0 (Linux; Android 10; Android SDK built for x86 Build/OSM1.180201.023) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.92 Mobile Safari/537.36" ||
           userAgent ===
-            "Mozilla/5.0 (iPhone; CPU iPhone OS 13_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/76.0.3809.123 Mobile/15E148 Safari/605.1")
+            "Mozilla/5.0 (iPhone; CPU iPhone OS 13_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/76.0.3809.123 Mobile/15E148 Safari/605.1" ||
+          userAgent.includes("TokenPocket"))
       ) {
         window.parent.location.href = window.self.location.href;
         return true;
