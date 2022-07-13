638728c83ca5cd684c5b010743702fd1a327e2db
JustinCast <justincastillovalladares@gmail.com>
2022-07-13 10:36:46 -0600

    feat: ens raffle

--- src/features/Raffles/raffles.js
+++ src/features/Raffles/raffles.js
@@ -21,14 +21,14 @@ const DataRow = styled.div`
   margin-bottom: 4px;
 `;
 
-const Icon = styled.div`
-  img {
-    width: 23px;
-  }
+// const Icon = styled.div`
+//   img {
+//     width: 23px;
+//   }
 
-  float: left;
-  width: 8%;
-`;
+//   float: left;
+//   width: 8%;
+// `;
 
 const Title = styled.div`
   padding-left: 2px;
@@ -60,7 +60,145 @@ const Raffles = () => {
             <Card className="mx-auto shadow-sm">
               <Card.Body>
                 <DataTitle className="d-flex flex-row justify-content-between">
-                  <a href="https://opensea.io/collection/shinsekaicorp" target="_blank">Shinsekai</a>
+                  <span>ENS Raffle</span>
+                  <span className="font-weight-bold">100</span>
+                </DataTitle>
+                <DataRow>
+                  <Title>0xfcf7c8fb47855e04a1bee503d1091b65359c6009</Title>
+                  <Value>30</Value>
+                </DataRow>
+                <DataRow>
+                  <Title>0xecac3e374e3dd1beef5734d8840673f7e7adab3a</Title>
+                  <Value>13</Value>
+                </DataRow>
+                <DataRow>
+                  <Title>0x23fd433bec56f7c62d7f087cd26bc8d1a6d8da78</Title>
+                  <Value>12</Value>
+                </DataRow>
+                <DataRow>
+                  <Title>0xb55794c3bef4651b6cbc78b64a2ef6c5c67837c3</Title>
+                  <Value>11</Value>
+                </DataRow>
+                <DataRow>
+                  <Title>0x594b7efdda12093e98945c89243b71ff79409e4a</Title>
+                  <Value>5</Value>
+                </DataRow>
+                <DataRow>
+                  <Title>0xd8ff659b400d546bb7c6f2dcb2193d36de91ce7b</Title>
+                  <Value>4</Value>
+                </DataRow>
+                <DataRow>
+                  <Title>0x21d6bd949118bf92f87dcf0aa5911fa26f0a007f</Title>
+                  <Value>2</Value>
+                </DataRow>
+                <DataRow>
+                  <Title>0x42a1de863683f3230568900ba23f86991d012f42</Title>
+                  <Value>2</Value>
+                </DataRow>
+                <DataRow>
+                  <Title>0x495690ecbd0d835815e13c0dc63f3e60e04601e8</Title>
+                  <Value>2</Value>
+                </DataRow>
+                <DataRow>
+                  <Title>0x55fd0a570711875587e216186b0ad91407208134</Title>
+                  <Value>2</Value>
+                </DataRow>
+                <DataRow>
+                  <Title>0x640a11d9a4b1db94b30a9cca25dc1e6870d7afc3</Title>
+                  <Value>2</Value>
+                </DataRow>
+                <DataRow>
+                  <Title>0x1adb86940e7c50fc6b82765cefee500feac5937e</Title>
+                  <Value>1</Value>
+                </DataRow>
+                <DataRow>
+                  <Title>0x2b5321c1afddfb6680aafff4df96a209e18dea1c</Title>
+                  <Value>1</Value>
+                </DataRow>
+                <DataRow>
+                  <Title>0x2caea78f301717711939313a8c72c26e596391dc</Title>
+                  <Value>1</Value>
+                </DataRow>
+                <DataRow>
+                  <Title>0x2e59387959e6c864d7a1761e4edb7803dc52a018</Title>
+                  <Value>1</Value>
+                </DataRow>
+                <DataRow>
+                  <Title>0x2ffbc532dc374b690f6af9a0d04f04f196eec2a3</Title>
+                  <Value>1</Value>
+                </DataRow>
+                <DataRow>
+                  <Title>0x4022f8ed1726584e91e8cdcbd85742b8db2bdfa3</Title>
+                  <Value>1</Value>
+                </DataRow>
+                <DataRow>
+                  <Title>0x4102a5a6b91883594f307812fde0d1d9b427356c</Title>
+                  <Value>1</Value>
+                </DataRow>
+                <DataRow>
+                  <Title>0x72506c2da622c5624878c7af2f813762bd0eb584</Title>
+                  <Value>1</Value>
+                </DataRow>
+                <DataRow>
+                  <Title>0x9996f0625a29ac20e06bac3e95e753b5bab2a6f6</Title>
+                  <Value>1</Value>
+                </DataRow>
+                <DataRow>
+                  <Title>0xa7cc2e956d269d4bcd4d52711c498727b7597a0a</Title>
+                  <Value>1</Value>
+                </DataRow>
+                <DataRow>
+                  <Title>0xa8172a3a34fdb1ef39ab4ebcfc8af096d7b583a4</Title>
+                  <Value>1</Value>
+                </DataRow>
+                <DataRow>
+                  <Title>0xdaa319496831b0babba5dd147849a51dcbd29114</Title>
+                  <Value>1</Value>
+                </DataRow>
+                <DataRow>
+                  <Title>0xe3fc8281bc7d1b78e67c2197b979849ed769edfa</Title>
+                  <Value>1</Value>
+                </DataRow>
+                <DataRow>
+                  <Title>0xe456f33a10c423c6e0296aa14c2762595ac255e7</Title>
+                  <Value>1</Value>
+                </DataRow>
+                <DataRow>
+                  <Title>0xfe0a17ff42e3f32a48d24c07483c04685f59d39c</Title>
+                  <Value>1</Value>
+                </DataRow>
+
+                <DataTitle className="d-flex flex-row justify-content-between">
+                  <a
+                    href="https://ipfs.everipedia.org/ipfs/QmWSQoDRfSdicRGVvuYS5yvvYQnNVuJcFY1m86FBPdvYXq"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Snapshot
+                  </a>
+                </DataTitle>
+                <DataTitle className="d-flex flex-row justify-content-between">
+                  <a
+                    href="https://polygonscan.com/address/0xb7185e8332fc2ff1a02664312288e11c39c0dbd0#events"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Onchain results
+                  </a>
+                </DataTitle>
+              </Card.Body>
+            </Card>
+
+            <Card className="mx-auto shadow-sm">
+              <Card.Body>
+                <DataTitle className="d-flex flex-row justify-content-between">
+                  <a
+                    href="https://opensea.io/collection/shinsekaicorp"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Shinsekai
+                  </a>
                   <span className="font-weight-bold">6</span>
                 </DataTitle>
                 <DataRow>
@@ -88,20 +226,36 @@ const Raffles = () => {
                   <Value>1</Value>
                 </DataRow>
                 <DataTitle className="d-flex flex-row justify-content-between">
-                  <a href="https://ipfs.everipedia.org/ipfs/QmWSQoDRfSdicRGVvuYS5yvvYQnNVuJcFY1m86FBPdvYXq"
-                     target="_blank">Snapshot</a>
+                  <a
+                    href="https://ipfs.everipedia.org/ipfs/QmWSQoDRfSdicRGVvuYS5yvvYQnNVuJcFY1m86FBPdvYXq"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Snapshot
+                  </a>
                 </DataTitle>
                 <DataTitle className="d-flex flex-row justify-content-between">
-                  <a href="https://polygonscan.com/address/0xb7185e8332fc2ff1a02664312288e11c39c0dbd0#events"
-                     target="_blank">Onchain results</a>
+                  <a
+                    href="https://polygonscan.com/address/0xb7185e8332fc2ff1a02664312288e11c39c0dbd0#events"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Onchain results
+                  </a>
                 </DataTitle>
               </Card.Body>
             </Card>
-            <br/>
+            <br />
             <Card className="mx-auto shadow-sm">
               <Card.Body>
                 <DataTitle className="d-flex flex-row justify-content-between">
-                  <a href="https://opensea.io/collection/supernormalbyzipcy" target="_blank">Zipcy&apos;s SuperNormal</a>
+                  <a
+                    href="https://opensea.io/collection/supernormalbyzipcy"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Zipcy&apos;s SuperNormal
+                  </a>
                   <span className="font-weight-bold">15</span>
                 </DataTitle>
                 <DataRow>
@@ -149,21 +303,36 @@ const Raffles = () => {
                   <Value>1</Value>
                 </DataRow>
                 <DataTitle className="d-flex flex-row justify-content-between">
-                  <a href="https://ipfs.everipedia.org/ipfs/Qmb7Kc2r7oH6ff5VdvV97ynuv9uVNXPVppjiMvkGF98F6v"
-                     target="_blank">Snapshot</a>
+                  <a
+                    href="https://ipfs.everipedia.org/ipfs/Qmb7Kc2r7oH6ff5VdvV97ynuv9uVNXPVppjiMvkGF98F6v"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Snapshot
+                  </a>
                 </DataTitle>
                 <DataTitle className="d-flex flex-row justify-content-between">
-                  <a href="https://polygonscan.com/address/0xb7185e8332fc2ff1a02664312288e11c39c0dbd0#events"
-                     target="_blank">Onchain results</a>
+                  <a
+                    href="https://polygonscan.com/address/0xb7185e8332fc2ff1a02664312288e11c39c0dbd0#events"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Onchain results
+                  </a>
                 </DataTitle>
               </Card.Body>
             </Card>
-            <br/>
+            <br />
             <Card className="mx-auto shadow-sm">
               <Card.Body>
                 <DataTitle className="d-flex flex-row justify-content-between">
-                  <a href="https://opensea.io/collection/juicy-js-journey-nft-drop" target="_blank">Juicy J&apos;s
-                    Journey NFT Drop</a>
+                  <a
+                    href="https://opensea.io/collection/juicy-js-journey-nft-drop"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Juicy J&apos;s Journey NFT Drop
+                  </a>
                   <span className="font-weight-bold">15</span>
                 </DataTitle>
                 <DataRow>
@@ -199,12 +368,22 @@ const Raffles = () => {
                   <Value>1</Value>
                 </DataRow>
                 <DataTitle className="d-flex flex-row justify-content-between">
-                  <a href="https://ipfs.everipedia.org/ipfs/QmWZ5MSWJgms6Gb6o1xyKD4VmvCzVGq941jxBYG97bFwFq"
-                     target="_blank">Snapshot</a>
+                  <a
+                    href="https://ipfs.everipedia.org/ipfs/QmWZ5MSWJgms6Gb6o1xyKD4VmvCzVGq941jxBYG97bFwFq"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Snapshot
+                  </a>
                 </DataTitle>
                 <DataTitle className="d-flex flex-row justify-content-between">
-                  <a href="https://polygonscan.com/address/0xb7185e8332fc2ff1a02664312288e11c39c0dbd0#events"
-                     target="_blank">Onchain results</a>
+                  <a
+                    href="https://polygonscan.com/address/0xb7185e8332fc2ff1a02664312288e11c39c0dbd0#events"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Onchain results
+                  </a>
                 </DataTitle>
               </Card.Body>
             </Card>
